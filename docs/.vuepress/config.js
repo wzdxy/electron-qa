e256c7f3dd1c94ed05ad099243d051b4b06e4746
wzdxy <zc_93@qq.com>
2020-04-28 20:52:38 +0800

    baidu 统计

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -46,4 +46,8 @@ module.exports = {
     //   ],
     // },
   },
+  head: [
+    // 百度统计
+    ['script', {}, `var _hmt = _hmt || [];(function() {var hm = document.createElement("script");hm.src = "https://hm.baidu.com/hm.js?b76bb9ba6c96f0fc7d6fc1edf2baecdb";var s = document.getElementsByTagName("script")[0];s.parentNode.insertBefore(hm, s);})();`]
+  ]
 };
